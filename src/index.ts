ea72f8f63a6509c02d36d0e1fe8bc4e94297d010
choi.jaehyeok <choi.jaehyeok@outlook.com>
2024-08-15 06:29:27 +0900

    refactor: Change folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export * from './application.js';
-export * from './class.js';
-export * from './entry-point.js';
-export { Handler, HandlerOptions } from './handler.js';
-export { Context, Event, ExecutionContext } from './parameters.js';
-export { ReflectedHandler, ReflectedMethod, ReflectedParameter } from './reflection.js';
+export * from './core.js';
